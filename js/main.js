9b0b12633270e8290a6a514809b377eb12fceb5f
Sven Fuchs <me@svenfuchs.com>
2014-02-22 21:08:17 +0100

    disable campaign

--- js/main.js
+++ js/main.js
@@ -21,42 +21,42 @@ $(document).ready(function() {
   });
 
   $('.donate-button').click(function(){
-    var self = $(this);
-    var amount = 0;
-    if (self.attr('data-custom')) {
-      amount = $('#custom-donation-amount').val();
-      if (amount === "") {
-        amount = 75;
-      }
-    } else {
-      amount = self.attr('data-amount');
-    }
-    amount = Math.abs(amount * 100);
+    // var self = $(this);
+    // var amount = 0;
+    // if (self.attr('data-custom')) {
+    //   amount = $('#custom-donation-amount').val();
+    //   if (amount === "") {
+    //     amount = 75;
+    //   }
+    // } else {
+    //   amount = self.attr('data-amount');
+    // }
+    // amount = Math.abs(amount * 100);
 
-    var package = self.attr('data-name');
+    // var package = self.attr('data-name');
 
-    var token = function(res) {
-      $('input[name="stripe_card_token"]').val(res.id);
-      $('input[name="amount"]').val(amount);
-      $('input[name="package"]').val(package);
-      $('form#stripe-form').submit();
-    };
-    var description = package == 'Custom' ? 'Your donation' : package + ' Sponsorship';
+    // var token = function(res) {
+    //   $('input[name="stripe_card_token"]').val(res.id);
+    //   $('input[name="amount"]').val(amount);
+    //   $('input[name="package"]').val(package);
+    //   $('form#stripe-form').submit();
+    // };
+    // var description = package == 'Custom' ? 'Your donation' : package + ' Sponsorship';
 
-    StripeCheckout.open({
-      key:         location.hostname == 'localhost' ? 'pk_test_oqAFNUozwz75eFQdaTyKojFt' : 'pk_live_yCtxaMJZgFa64UQKWEVsrL5h',
-      address:     false,
-      amount:      amount,
-      currency:    'usd',
-      name:        'Rails Girls Summer of Code',
-      description: description,
-      panelLabel:  'Donate',
-      token:       token
-    });
+    // StripeCheckout.open({
+    //   key:         location.hostname == 'localhost' ? 'pk_test_oqAFNUozwz75eFQdaTyKojFt' : 'pk_live_yCtxaMJZgFa64UQKWEVsrL5h',
+    //   address:     false,
+    //   amount:      amount,
+    //   currency:    'usd',
+    //   name:        'Rails Girls Summer of Code',
+    //   description: description,
+    //   panelLabel:  'Donate',
+    //   token:       token
+    // });
 
-    return false;
+    // return false;
   });
-  
+
   // Mobile navigation toggle
   //$('.nav-primary > ul').addClass('hidden');
   $('.hamburger').click(function() {
